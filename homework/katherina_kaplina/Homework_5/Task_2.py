475ea479b8466125293b8e4b3bc2c379e6338def
Katherina_Kaplina <kaplinakatrin@gmail.com>
2024-05-22 19:51:28 +0300

    fixed issue in task 2 for homework 5

--- homework/katherina_kaplina/Homework_5/Task_2.py
+++ homework/katherina_kaplina/Homework_5/Task_2.py
@@ -2,10 +2,10 @@ output_1 = 'результат операции: 42'
 output_2 = 'результат операции: 514'
 output_3 = 'результат работы программы: 9'
 
-index_1 = output_1.index('42')
-index_2 = output_2.index('514')
-index_3 = output_3.index('9')
+index_1 = output_1.index(':')
+index_2 = output_2.index(':')
+index_3 = output_3.index(':')
 
-print(int(output_1[index_1:]) + 10)
-print(int(output_2[index_2:]) + 10)
-print(int(output_3[index_3:]) + 10)
+print(int(output_1[index_1+1:]) + 10)
+print(int(output_2[index_2+1:]) + 10)
+print(int(output_3[index_3+1:]) + 10)
