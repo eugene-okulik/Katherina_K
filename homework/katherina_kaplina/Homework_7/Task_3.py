94ce06a811ece354ce73334c0e2967d01b7d9988
Katherina_Kaplina <kaplinakatrin@gmail.com>
2024-05-27 14:03:04 +0300

    fixed formatting issue in task 3 for homework 7

--- homework/katherina_kaplina/Homework_7/Task_3.py
+++ homework/katherina_kaplina/Homework_7/Task_3.py
@@ -3,9 +3,11 @@ output_2 = 'результат операции: 514'
 output_3 = 'результат работы программы: 9'
 output_4 = 'результат: 2'
 
+
 def calculation(parameter):
     number = int(parameter.split(': ')[1])
     return number + 10
+
 
 print(calculation(output_1))
 print(calculation(output_2))
