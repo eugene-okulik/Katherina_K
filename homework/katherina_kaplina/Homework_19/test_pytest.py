ab13a0cd27b2f0eee771cfe8c6016eb75f185033
Katherina_Kaplina <kaplinakatrin@gmail.com>
2024-07-05 00:31:43 +0300

    added Homework 19

--- homework/katherina_kaplina/Homework_19/test_pytest.py
+++ homework/katherina_kaplina/Homework_19/test_pytest.py
@@ -1,34 +1,9 @@
 import requests
+import pytest
 
 
-def add_object():
-    body = {
-        "name": "Apple MacBook Pro 2024 Kate",
-        "data": {
-            "year": 2024,
-            "price": 200,
-            "CPU model": "Intel Core i9",
-            "Hard disk size": "1 TB"
-        }
-    }
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(
-        'https://api.restful-api.dev/objects',
-        json=body,
-        headers=headers
-    )
-    assert response.status_code == 200, 'Status code is incorrect'
-    assert response.json()['name'] == 'Apple MacBook Pro 2024 Kate', 'Not correct name'
-    assert response.json()['data']['year'] == 2024, 'Not correct year'
-    assert response.json()['data']['price'] == 200, 'Not correct price'
-    assert response.json()['data']['CPU model'] == "Intel Core i9", 'Not correct CPU model'
-    assert response.json()['data']['Hard disk size'] == "1 TB", 'Not correct Hard disk size'
-
-
-add_object()
-
-
-def new_object():
+@pytest.fixture()
+def new_object_id():
     body = {
         "name": "NEW - Apple MacBook Pro 2024 Kate",
         "data": {
@@ -44,15 +19,48 @@ def new_object():
         json=body,
         headers=headers
     )
-    return response.json()['id']
-
-
-def clear(object_id):
+    object_id = response.json()['id']
+    yield object_id
     requests.delete(f'https://api.restful-api.dev/objects/{object_id}')
 
 
-def update_object():
-    object_id = new_object()
+@pytest.fixture(scope='session')
+def test_info():
+    print('Starting testing')
+    yield
+    print('Testing completed')
+
+
+@pytest.mark.parametrize('name', ['', '    ', '!@#$^'])
+def test_add_object(test_info, name):
+    print('\nbefore test')
+    body = {
+        "name": "Apple MacBook Pro 2024 Kate",
+        "data": {
+            "year": 2024,
+            "price": 200,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    }
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(
+        'https://api.restful-api.dev/objects',
+        json=body,
+        headers=headers
+    )
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Apple MacBook Pro 2024 Kate'
+    assert response.json()['data']['year'] == 2024
+    assert response.json()['data']['price'] == 200
+    assert response.json()['data']['CPU model'] == "Intel Core i9"
+    assert response.json()['data']['Hard disk size'] == "1 TB"
+    print('after test')
+
+
+@pytest.mark.critical
+def test_update_object(new_object_id):
+    print('\nbefore test')
     body = {
         "name": "Apple MacBook Pro 2024 Kate - Last model",
         "data": {
@@ -65,25 +73,23 @@ def update_object():
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.put(
-        f'https://api.restful-api.dev/objects/{object_id}',
+        f'https://api.restful-api.dev/objects/{new_object_id}',
         json=body,
         headers=headers
     )
-    assert response.status_code == 200, 'Status code is incorrect'
-    assert response.json()['name'] == 'Apple MacBook Pro 2024 Kate - Last model', 'Not correct name'
-    assert response.json()['data']['year'] == 2025, 'Not correct year'
-    assert response.json()['data']['price'] == 2000, 'Not correct price'
-    assert response.json()['data']['CPU model'] == "Intel Core i9", 'Not correct CPU model'
-    assert response.json()['data']['Hard disk size'] == "10 TB", 'Not correct Hard disk size'
-    assert response.json()['data']['color'] == "white", 'Not correct color'
-    clear(object_id)
+    assert response.status_code == 200
+    assert response.json()['name'] == 'Apple MacBook Pro 2024 Kate - Last model'
+    assert response.json()['data']['year'] == 2025
+    assert response.json()['data']['price'] == 2000
+    assert response.json()['data']['CPU model'] == "Intel Core i9"
+    assert response.json()['data']['Hard disk size'] == "10 TB"
+    assert response.json()['data']['color'] == "white"
+    print('after test')
 
 
-update_object()
-
-
-def partially_update_object():
-    object_id = new_object()
+@pytest.mark.medium
+def test_partially_update_object(new_object_id):
+    print('\nbefore test')
     body = {
         "data": {
             "year": 2030,
@@ -95,27 +101,22 @@ def partially_update_object():
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.patch(
-        f'https://api.restful-api.dev/objects/{object_id}', 
+        f'https://api.restful-api.dev/objects/{new_object_id}', 
         json=body,
         headers=headers
     )
-    assert response.status_code == 200, 'Status code is incorrect'
+    assert response.status_code == 200
     assert response.json()['name'] == 'NEW - Apple MacBook Pro 2024 Kate', 'Not correct name'
     assert response.json()['data']['year'] == 2030, 'Not correct year'
     assert response.json()['data']['price'] == 20000, 'Not correct price'
     assert response.json()['data']['CPU model'] == "Intel Core i9", 'Not correct CPU model'
     assert response.json()['data']['Hard disk size'] == "10 TB", 'Not correct Hard disk size'
     assert response.json()['data']['color'] == "gold", 'Not correct color'
-    clear(object_id)
+    print('after test')
 
 
-partially_update_object()
-
-
-def delete_object():
-    object_id = new_object()
-    response = requests.delete(f'https://api.restful-api.dev/objects/{object_id}')
+def test_delete_object(new_object_id):
+    print('\nbefore test')
+    response = requests.delete(f'https://api.restful-api.dev/objects/{new_object_id}')
     assert response.status_code == 200, 'Status code is incorrect'
-
-
-delete_object()
+    print('after test')
